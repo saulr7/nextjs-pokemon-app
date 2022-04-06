a2d912c11dcc9946973d04dd0d1c505ce5587729
Saul Ramirez <fernando.ramire@hotmail.com>
2022-04-06 10:12:48 -0600

    :recycle: refactoring request

--- utils/pokemonSevices.ts
+++ utils/pokemonSevices.ts
@@ -2,14 +2,20 @@ import { pokeApi } from "../api";
 import { Pokemon } from "../interfaces";
 
 const getPokemonInfo = async (id: string) => {
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`);
 
-  const pokemon = {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-  };
-  return pokemon
+  try {
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`);
+
+    const pokemon = {
+      id: data.id,
+      name: data.name,
+      sprites: data.sprites,
+    };
+    return pokemon
+  } catch (error) {
+    return null
+  }
+
 
 }
 
